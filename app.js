d3a499179bdab105834318d4ebaffd78c43d5331
Cassio Henrique Lima Fernandes <cassiofernandes298@gmail.com>
2020-12-08 23:03:20 -0300

    Genetic Algorithm working + Integration with front-end unfinished

---
+++ app.js
@@ -0,0 +1,48 @@
+const GeneticAlgorithm = require('./scripts/genetic') //Codigo Altgoritmo Genético
+var geneticAlgorithmInstance;
+
+//console.log("STARTING: ", geneticAlgorithmInstance.population())
+// for (let index = 0; index < 3; index++) {
+//     geneticalgorithm.geneticalgorithm.evolve()
+//     console.log("EPOCH "+ index +": ", geneticalgorithm.geneticalgorithm.population())
+//     var population = geneticalgorithm.geneticalgorithm.population();
+//     population.map((element) => {element.score = 0; return element;})
+//     geneticalgorithm.geneticalgorithm = geneticalgorithm.geneticalgorithm.clone({population: population})
+// }
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(express.static('public'));// Usado para localizar arquivos estáticos na pasta public
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Rotas
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/start/:size', async function (req, res) {
+    console.log(req.params.size)
+    geneticAlgorithmInstance = await GeneticAlgorithm.createGeneticAlgorithm(req.params.size);
+    var population = geneticAlgorithmInstance.population();
+    console.log("STARTING: ", geneticAlgorithmInstance.population())
+    res.json(population);
+})
+
+app.post('/evolve', async function(req, res) {
+    console.log("TO EVOLVE: ", req.body.population)
+    var population = req.body.population;
+    console.log("POPULATION BEFORE CLONE: ", geneticAlgorithmInstance.population())
+    geneticAlgorithmInstance = await geneticAlgorithmInstance.clone({population: population})
+    console.log("POPULATION AFTER CLONE: ", geneticAlgorithmInstance.population())
+    await geneticAlgorithmInstance.evolve()
+    population = await geneticAlgorithmInstance.population();
+    console.log("EVOLVED: ", population)
+    res.json(population)
+})
+
+app.listen(8081 , function() {
+    console.log("Servidor rodando na url http://localhost:8081...")
+}) //Tem que ser a ultima linha
---
+++ scripts/genetic.js
@@ -0,0 +1,85 @@
+var geneticAlgorithmConstructor = require('geneticalgorithm');
+
+// ** HELPER FUNCTIONS **
+function cloneJSON( item ) { return JSON.parse ( JSON.stringify ( item ) ) }
+
+async function createGeneticAlgorithm(size){
+    var population = await createStartingPopulation(size);
+    var config = {
+        mutationFunction: mutationFunction,
+        crossoverFunction: crossoverFunction,
+        fitnessFunction: fitnessFunction,
+        //doesABeatBFunction: yourCompetitionFunction,
+        population: population,
+        populationSize: size 	// defaults to 100
+    }
+    
+    var geneticalgorithm = geneticAlgorithmConstructor( config )
+    return geneticalgorithm;
+}
+
+function createStartingPopulation(size){
+    var randomPopulation = [];
+    for (let index = 0; index < size; index++) {
+        var randomPhenotype = createRandomPhenotype();
+        randomPopulation.push(randomPhenotype)
+    }
+
+    return randomPopulation;
+}
+
+function createRandomPhenotype(){
+    var newPhenotype = {};
+
+    newPhenotype.score = 0;
+    newPhenotype.velocidade = (Math.round(Math.random() * 3));
+    newPhenotype.tamanho = (Math.random() < .5 ? -1 : 1) * (Math.round(Math.random() * 16));
+    newPhenotype.sentidos = 50 + (Math.random() < .5 ? -1 : 1) * (Math.round(Math.random() * 32));
+    newPhenotype.stamina = 100 + (Math.random() < .5 ? -1 : 1) * (Math.round(Math.random() * 30));
+
+    return newPhenotype;
+}
+
+// ** GENETIC ALGORITHM ** 
+function mutationFunction(phenotype) {
+    var resultPhenotype = cloneJSON(phenotype)
+
+    resultPhenotype.velocidade += (Math.random() < .5 ? -1 : 1) * (Math.round(Math.random() * 2));
+    resultPhenotype.tamanho += (Math.random() < .5 ? -1 : 1) * (Math.round(Math.random() * 4));
+    resultPhenotype.sentidos += (Math.random() < .5 ? -1 : 1) * (Math.round(Math.random() * 8));
+    resultPhenotype.stamina += (Math.random() < .5 ? -1 : 1) * (Math.round(Math.random() * 10));
+
+    //console.log("MUTATIONS: ", resultPhenotype);
+    
+    return resultPhenotype;
+}
+
+//Tem que retornar um array
+function crossoverFunction(a, b) {
+    let crossed = cloneJSON(a)
+    crossed.velocidade = Math.random() < .5 ? a.velocidade : b.velocidade;
+    crossed.tamanho = Math.random() < .5 ? a.tamanho : b.tamanho;
+    crossed.sentidos = Math.random() < .5 ? a.sentidos : b.sentidos;
+    crossed.stamina = Math.random() < .5 ? a.stamina : b.stamina;
+    
+    //console.log("CROSSED: ", crossed);
+    
+    return [crossed];
+}
+
+function fitnessFunction(phenotype) {
+    
+    var fitness;
+    
+    if(phenotype == null){
+        fitness = 0;
+    } else{
+        fitness = phenotype.score;
+    }
+    
+    //console.log("FITNESS: ", fitness);
+    
+    return fitness;
+}
+
+module.exports = {createGeneticAlgorithm};
